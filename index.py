6d484672ba599fbfc6d9aa9e15b98008c10e2109
KumkumDutta <112754510+kumkumdutta@users.noreply.github.com>
2023-08-07 11:21:49 +0530

    set 2 updated

--- index.py
+++ index.py
@@ -172,3 +172,118 @@ def generate_fibonacci_sequence(n):
 n = 10
 fib_sequence = generate_fibonacci_sequence(n)
 print(fib_sequence)
+
+
+# Write a program to print the following number pattern using a loop.
+
+def print_number_pattern(rows):
+    for i in range(1, rows + 1):
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        print()
+
+
+rows = 5
+print_number_pattern(rows)
+
+
+# Write a program to display only those numbers from a [list](https://pynative.com/python-lists/) that satisfy the following conditions
+
+# - The number must be divisible by five
+# - If the number is greater than 150, then skip it and move to the next number
+# - If the number is greater than 500, then stop the loop
+
+
+def filter_numbers(numbers):
+    for num in numbers:
+        if num > 500:
+            break  
+        if num % 5 == 0 and num <= 150:
+            print(num)
+
+
+numbers = [12, 75, 150, 180, 145, 525, 50]
+
+
+filter_numbers(numbers)
+
+
+
+# Given two strings, s1 and s2. Write a program to create a new string s3 by appending s2 in the middle of s1.
+
+
+def append_in_middle(s1, s2):
+    middle_index = len(s1) // 2
+    s3 = s1[:middle_index] + s2 + s1[middle_index:]
+    return s3
+
+s1 = "Ault"
+s2 = "Kelly"
+s3 = append_in_middle(s1, s2)
+print(s3)
+
+
+# Given string contains a combination of the lower and upper case letters. Write a program to arrange the characters of a string so that all lowercase letters should come first.
+
+
+def arrange_lowercase_first(s):
+    lowercase = ""
+    uppercase = ""
+
+    for char in s:
+        if char.islower():
+            lowercase += char
+        else:
+            uppercase += char
+
+    arranged_string = lowercase + uppercase
+    return arranged_string
+
+str1 = "PyNaTive"
+arranged_str = arrange_lowercase_first(str1)
+print(arranged_str)
+
+
+
+
+# Write a program to add two lists index-wise. Create a new list that contains the 0th index item from both the list, then the 1st index item, and so on till the last element. any leftover items will get added at the end of the new list.
+
+
+def add_lists_indexwise(list1, list2):
+    combined_list = [x + y for x, y in zip(list1, list2)]
+    remaining_items = list1[len(list2):] + list2[len(list1):]
+    combined_list += remaining_items
+    return combined_list
+
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+
+result_list = add_lists_indexwise(list1, list2)
+print(result_list)
+
+
+
+# 6
+
+list1 = ["Hello ", "take "]
+list2 = ["Dear", "Sir"]
+
+concatenated_list = []
+
+for item1 in list1:
+    for item2 in list2:
+        concatenated_list.append(item1 + item2)
+
+print(concatenated_list)
+
+
+# 7
+
+list1 = [10, 20, 30, 40]
+list2 = [100, 200, 300, 400]
+
+for item1, item2 in zip(list1, reversed(list2)):
+    print(item1, item2)
+
+
+
